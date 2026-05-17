23099e9de6963faf1c1fad138b96f56793e1fadf
Omer <babayev.omr.23@gmail.com>
2026-05-17 10:16:36 +0400

    add

--- frontend/app/src/main.tsx
+++ frontend/app/src/main.tsx
@@ -1,8 +1,8 @@
+import './i18n'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import './i18n'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
