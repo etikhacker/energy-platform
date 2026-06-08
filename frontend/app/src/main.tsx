13fa5cdb3ea50a81cd043f2f0adb7b83045038f4
Omer <babayev.omr.23@gmail.com>
2026-06-08 21:22:35 +0400

    Fix i18n dashboard loading

--- frontend/app/src/main.tsx
+++ frontend/app/src/main.tsx
@@ -1,10 +1,9 @@
-import './i18n'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import './i18n'; // Bu sətir i18n konfiqurasiyasını qlobal olaraq işə salır
+import './i18n'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
